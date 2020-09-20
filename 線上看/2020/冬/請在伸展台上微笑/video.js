89efc2bd2546d9e9d16a2c5ee3fe88fbf692bf5c
gokazeacg <56425629+gokazeacg@users.noreply.github.com>
2020-09-20 21:56:42 +0800

    請在伸展台上微笑

---
+++ 線上看/2020/冬/請在伸展台上微笑/video.js
@@ -0,0 +1,110 @@
+document.getElementById("video1").src = "";
+document.getElementById("video2").src = "";
+document.getElementById("video3").src = "";
+document.getElementById("video4").src = "";
+document.getElementById("video5").src = "";
+document.getElementById("video6").src = "";
+document.getElementById("video7").src = "";
+document.getElementById("video8").src = "";
+document.getElementById("video9").src = "";
+document.getElementById("video10").src = "";
+document.getElementById("video11").src = "";
+document.getElementById("video12").src = "";
+
+//主要節點
+function video1_1() {
+    document.getElementById("video1").src = "";
+}
+
+function video2_1() {
+    document.getElementById("video2").src = "";
+}
+
+function video3_1() {
+    document.getElementById("video3").src = "";
+}
+
+function video4_1() {
+    document.getElementById("video4").src = "";
+}
+
+function video5_1() {
+    document.getElementById("video5").src = "";
+}
+
+function video6_1() {
+    document.getElementById("video6").src = "";
+}
+
+function video7_1() {
+    document.getElementById("video7").src = "";
+}
+
+function video8_1() {
+    document.getElementById("video8").src = "";
+}
+
+function video9_1() {
+    document.getElementById("video9").src = "";
+}
+
+function video10_1() {
+    document.getElementById("video10").src = "";
+}
+
+function video11_1() {
+    document.getElementById("video11").src = "";
+}
+
+function video12_1() {
+    document.getElementById("video12").src = "";
+}
+
+//備用節點
+function video1_2() {
+    document.getElementById('video1').src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video2_2() {
+    document.getElementById("video2").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video3_2() {
+    document.getElementById("video3").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video4_2() {
+    document.getElementById("video4").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video5_2() {
+    document.getElementById("video5").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video6_2() {
+    document.getElementById("video6").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video7_2() {
+    document.getElementById("video7").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video8_2() {
+    document.getElementById("video8").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video9_2() {
+    document.getElementById("video9").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video10_2() {
+    document.getElementById("video10").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video11_2() {
+    document.getElementById("video11").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video12_2() {
+    document.getElementById("video12").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
