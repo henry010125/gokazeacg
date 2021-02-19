d35e19a6298357cd6cd5bd18d9f4ae826bfc54ee
gokazeacg <56425629+gokazeacg@users.noreply.github.com>
2021-02-19 09:06:39 +0800

    Create video.js

---
+++ 線上看/2020/春/富豪刑警/video.js
@@ -0,0 +1,105 @@
+document.getElementById("video1").src = "https://www.googleapis.com/drive/v3/files/16z28zbtStyV5N3j6TQL1Jn3mGJ0UO5Bs?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video2").src = "https://www.googleapis.com/drive/v3/files/1DyQFISOJqrr498mCAe58sQC1KzSi54iM?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video3").src = "https://www.googleapis.com/drive/v3/files/1JC2t9pr9RlZ_tnGtJGfoh6uhC14IVm7z?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video4").src = "https://www.googleapis.com/drive/v3/files/1Iy-XcIyi8FYF6tgtGvtJ7JI1mBB9-jJa?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video5").src = "https://www.googleapis.com/drive/v3/files/1ycG02Sg_Vg1sI3tF-mb3y2ZDwu2S-ubF?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video6").src = "https://www.googleapis.com/drive/v3/files/1fBRONKWNISP__IPjxRm7akmEe8-SNqyW?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video7").src = "https://www.googleapis.com/drive/v3/files/1j_fK6dtb_oCgelDA6s0vNS8J8Ag_vgXq?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video8").src = "https://www.googleapis.com/drive/v3/files/1yHtflmU7k5ZDOTBGA6nTKZmuL2TxREii?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video9").src = "https://www.googleapis.com/drive/v3/files/1ygPl_9n5Sk-zLX8NXq4UlfJtsSjLAQ01?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video10").src = "https://www.googleapis.com/drive/v3/files/1PuEcI4k7nWxuciUvJFCRCqCdunaQy32A?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+document.getElementById("video11").src = "https://www.googleapis.com/drive/v3/files/1fNA-RO0V7T8CW318wiDBwjBFWrZtannR?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+
+//主要節點
+function video1_1() {
+    document.getElementById("video1").src = "https://www.googleapis.com/drive/v3/files/16z28zbtStyV5N3j6TQL1Jn3mGJ0UO5Bs?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video2_1() {
+    document.getElementById("video2").src = "https://www.googleapis.com/drive/v3/files/1DyQFISOJqrr498mCAe58sQC1KzSi54iM?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video3_1() {
+    document.getElementById("video3").src = "https://www.googleapis.com/drive/v3/files/1JC2t9pr9RlZ_tnGtJGfoh6uhC14IVm7z?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video4_1() {
+    document.getElementById("video4").src = "https://www.googleapis.com/drive/v3/files/1Iy-XcIyi8FYF6tgtGvtJ7JI1mBB9-jJa?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video5_1() {
+    document.getElementById("video5").src = "https://www.googleapis.com/drive/v3/files/1ycG02Sg_Vg1sI3tF-mb3y2ZDwu2S-ubF?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video6_1() {
+    document.getElementById("video6").src = "https://www.googleapis.com/drive/v3/files/1fBRONKWNISP__IPjxRm7akmEe8-SNqyW?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video7_1() {
+    document.getElementById("video7").src = "https://www.googleapis.com/drive/v3/files/1j_fK6dtb_oCgelDA6s0vNS8J8Ag_vgXq?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video8_1() {
+    document.getElementById("video8").src = "https://www.googleapis.com/drive/v3/files/1yHtflmU7k5ZDOTBGA6nTKZmuL2TxREii?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video9_1() {
+    document.getElementById("video9").src = "https://www.googleapis.com/drive/v3/files/1ygPl_9n5Sk-zLX8NXq4UlfJtsSjLAQ01?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video10_1() {
+    document.getElementById("video10").src = "https://www.googleapis.com/drive/v3/files/1PuEcI4k7nWxuciUvJFCRCqCdunaQy32A?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video11_1() {
+    document.getElementById("video11").src = "https://www.googleapis.com/drive/v3/files/1fNA-RO0V7T8CW318wiDBwjBFWrZtannR?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media";
+}
+
+function video24_1() {
+    document.getElementById("video24").src = "";
+}
+
+//備用節點
+function video1_2() {
+    document.getElementById('video1').src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video2_2() {
+    document.getElementById("video2").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video3_2() {
+    document.getElementById("video3").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video4_2() {
+    document.getElementById("video4").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video5_2() {
+    document.getElementById("video5").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video6_2() {
+    document.getElementById("video6").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video7_2() {
+    document.getElementById("video7").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video8_2() {
+    document.getElementById("video8").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video9_2() {
+    document.getElementById("video9").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video10_2() {
+    document.getElementById("video10").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
+
+function video11_2() {
+    document.getElementById("video11").src = "https://www.googleapis.com/drive/v3/files/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+}
