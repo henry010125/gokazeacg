dbdf1d4597577d5aff4800bdd4486a7de6f0cb8d
gokazeacg <56425629+gokazeacg@users.noreply.github.com>
2021-04-03 20:20:46 +0800

    Update video.js

--- 線上看/2020/冬/達爾文遊戲/video.js
+++ 線上看/2020/冬/達爾文遊戲/video.js
@@ -6,19 +6,19 @@ function video1() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_01.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_01.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_01.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_01.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_01.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_01.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_01.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_01.mp4';
 }
 
 function video2() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
-    document.getElementById('captions').src = './達爾文遊戲_02.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_02.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_02.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_02.mp4';
+    document.getElementById('captions').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/Subtitle/達爾文遊戲_HEVC-10bit_1080P_nosub_02.vtt';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_02.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_02.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_02.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_02.mp4';
 }
 
@@ -26,9 +26,9 @@ function video3() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_03.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_03.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_03.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_03.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_03.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_03.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_03.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_03.mp4';
 }
 
@@ -36,9 +36,9 @@ function video4() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_04.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_04.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_04.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_04.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_04.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_04.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_04.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_04.mp4';
 }
 
@@ -46,9 +46,9 @@ function video5() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_05.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_05.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_05.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_05.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_05.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_05.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_05.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_05.mp4';
 }
 
@@ -56,9 +56,9 @@ function video6() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_06.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_06.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_06.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_06.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_06.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_06.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_06.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_06.mp4';
 }
 
@@ -66,9 +66,9 @@ function video7() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_07.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_07.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_07.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_07.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_07.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_07.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_07.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_07.mp4';
 }
 
@@ -76,9 +76,9 @@ function video8() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_08.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_08.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_08.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_08.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_08.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_08.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_08.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_08.mp4';
 }
 
@@ -86,9 +86,9 @@ function video9() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_09.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_09.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_09.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_09.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_09.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_09.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_09.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_09.mp4';
 }
 
@@ -96,9 +96,9 @@ function video10() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_10.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_10.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_10.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_10.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_10.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_10.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_10.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_10.mp4';
 }
 
@@ -106,8 +106,8 @@ function video11() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('captions').src = './達爾文遊戲_11.vtt';
-    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_360P_nosub_11_end.mp4';
-    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_720P_nosub_11_end.mp4';
-    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/達爾文遊戲_1080P_nosub_11_end.mp4';
+    document.getElementById('360P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/360P/達爾文遊戲_360P_nosub_11_end.mp4';
+    document.getElementById('720P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/720P/達爾文遊戲_720P_nosub_11_end.mp4';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Stream/1080P/達爾文遊戲_1080P_nosub_11_end.mp4';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/達爾文遊戲/Download/達爾文遊戲_HEVC-10bit_1080P_nosub_dl_11_end.mp4';
 }
