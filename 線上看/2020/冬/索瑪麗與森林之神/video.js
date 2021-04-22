3cffe16ecffe6df9842e3a0a6b1c461bd219b698
gokazeacg <gokazeacg@gmail.com>
2021-04-22 14:21:53 +0800

    Update to new version

--- 線上看/2020/冬/索瑪麗與森林之神/video.js
+++ 線上看/2020/冬/索瑪麗與森林之神/video.js
@@ -1,109 +1,105 @@
-document.getElementById("video1").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EXNK5IEiujdBt9k1u3RXht4BS7BH9EJnZ5a7XkqTbEm40g?download=1";
-document.getElementById("video2").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EaT6o38Wc_NDnZUUHU_i4B4BDxUXl5lcX4bxfXgrSdcObQ?download=1";
-document.getElementById("video3").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EcGa_9RTF9pIhm1dtlLuqeMBuvoHCpSQMjNahQMDciS1_w?download=1";
-document.getElementById("video4").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ed49sYmlOhVKjDfFdTIDODUBnPp96LFBNrv-DcAQ0N0Ssg?download=1";
-document.getElementById("video5").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EUagzoARxppKuRpBr30J5l4BwIJHqEMlvlA2odDfwTiMxQ?download=1";
-document.getElementById("video6").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ea7b4CMsKINAoR86Fdvcm5sBmXLqitpaXKaZ41T4uinI1w?download=1";
-document.getElementById("video7").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EbnsFijs7glOtBZvv6ygTdIBhnuwL3lUHWj-gm6paj-GnA?download=1";
-document.getElementById("video8").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EckEev76A1pIuLnVDkqwJwIBq4-NjnAGfJelRZO0BEJvHw?download=1";
-document.getElementById("video9").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Eci6YWWuY9FKuNGD0kxcXl8Bb-I0V7HCh5lhUW7sATbDMQ?download=1";
-document.getElementById("video10").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ed4iM-1VMHlJh-n4U4jvQ9ABEGgTXYtoq-Oc7mSywOvYaA?download=1";
-document.getElementById("video11").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EX16FriURwFMseylJT47seQBYiOSB3ZfcWUY_PhX1-IKcQ?download=1";
-document.getElementById("video12").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/ERfU8VNg7OhOv_p8ro8FP9EBqG69H-i0TIyGyw4FfhlxCg?download=1";
-
-//主要節點
-function video1_1() {
-    document.getElementById("video1").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EXNK5IEiujdBt9k1u3RXht4BS7BH9EJnZ5a7XkqTbEm40g?download=1";
+function dl() {
+    location.href='https://www.googleapis.com/drive/v3/files/1IIaUA5ZEJ1L6pCL_N_tczO-CItw2Xv--?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media'
 }
 
-function video2_1() {
-    document.getElementById("video2").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EaT6o38Wc_NDnZUUHU_i4B4BDxUXl5lcX4bxfXgrSdcObQ?download=1";
+function home() {
+    document.getElementById('home').style.display = 'unset';
+    document.getElementById('video').style.display = 'none';
+    document.getElementById('Episode').innerHTML = '故事大綱';
 }
 
-function video3_1() {
-    document.getElementById("video3").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EcGa_9RTF9pIhm1dtlLuqeMBuvoHCpSQMjNahQMDciS1_w?download=1";
+function video1() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#01 旅行的父女';    
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_01.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_01.mkv';
 }
 
-function video4_1() {
-    document.getElementById("video4").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ed49sYmlOhVKjDfFdTIDODUBnPp96LFBNrv-DcAQ0N0Ssg?download=1";
+function video2() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#02 野草與鬼族之家';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_02.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_02.mkv';
 }
 
-function video5_1() {
-    document.getElementById("video5").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EUagzoARxppKuRpBr30J5l4BwIJHqEMlvlA2odDfwTiMxQ?download=1";
+function video3() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#03 洞穴之下的海洋';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_03.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_03.mkv';
 }
 
-function video6_1() {
-    document.getElementById("video6").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ea7b4CMsKINAoR86Fdvcm5sBmXLqitpaXKaZ41T4uinI1w?download=1";
+function video4() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#04 如願之花與許下的約定';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_04.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_04.mkv';
 }
 
-function video7_1() {
-    document.getElementById("video7").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EbnsFijs7glOtBZvv6ygTdIBhnuwL3lUHWj-gm6paj-GnA?download=1";
+function video5() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#05 飄搖之鳥';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_05.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_05.mkv';
 }
 
-function video8_1() {
-    document.getElementById("video8").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EckEev76A1pIuLnVDkqwJwIBq4-NjnAGfJelRZO0BEJvHw?download=1";
+function video6() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#06 棲息於地的花朵仰望鳥兒';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_06.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_06.mkv';
 }
 
-function video9_1() {
-    document.getElementById("video9").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Eci6YWWuY9FKuNGD0kxcXl8Bb-I0V7HCh5lhUW7sATbDMQ?download=1";
+function video7() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#07 追逐魔女的蹤跡';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_07.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_07.mkv';
 }
 
-function video10_1() {
-    document.getElementById("video10").src = https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/Ed4iM-1VMHlJh-n4U4jvQ9ABEGgTXYtoq-Oc7mSywOvYaA?download=1";
+function video8() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#08 與祈願訴說那段相遇與羈絆';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_08.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_08.mkv';
 }
 
-function video11_1() {
-    document.getElementById("video11").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/EX16FriURwFMseylJT47seQBYiOSB3ZfcWUY_PhX1-IKcQ?download=1";
-}
-function video1_1() {
-    document.getElementById("video12").src = "https://angelcquinonez-my.sharepoint.com/:v:/g/personal/animeonline_office_jimschool_org/ERfU8VNg7OhOv_p8ro8FP9EBqG69H-i0TIyGyw4FfhlxCg?download=1";
-}
-
-//備用節點
-function video1_2() {
-    document.getElementById('video1').src = "https://www.googleapis.com/drive/v3/files/1pcuWiLclFhBPaLzPIVOa2GqzjKMEmSP4?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+function video9() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#09 日日夜夜的回憶';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_09.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_09.mkv';
 }
 
-function video2_2() {
-    document.getElementById("video2").src = "https://www.googleapis.com/drive/v3/files/15fVkKVNWr6k0iulnb-V4sAc7oLkC5-I7?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+function video10() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#10 幼子與綠之城塞';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_10.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_10.mkv';
 }
 
-function video3_2() {
-    document.getElementById("video3").src = "https://www.googleapis.com/drive/v3/files/1V0Epbnv3x2zAlW1RgFp8JraHsB57hec2?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+function video11() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#11 保護者與剝奪者';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_11.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_11.mkv';
 }
 
-function video4_2() {
-    document.getElementById("video4").src = "https://www.googleapis.com/drive/v3/files/1KqNwVjnGmObQ-p_xdLO5CpO8BKZja4qT?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video5_2() {
-    document.getElementById("video5").src = "https://www.googleapis.com/drive/v3/files/1BYzoHuGOV9NiXo_zWwL2I6sWpvHSg1c7?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video6_2() {
-    document.getElementById("video6").src = "https://www.googleapis.com/drive/v3/files/1AAEYt68cwrgJM1R5r608keo_37p9gDNR?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video7_2() {
-    document.getElementById("video7").src = "https://www.googleapis.com/drive/v3/files/1aggrfv03OMgEXhJvKAGPi2lF6Om_7oQC?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video8_2() {
-    document.getElementById("video8").src = "https://www.googleapis.com/drive/v3/files/17fCJf3iX0pQhoJA0jx0CaH77l9BMuT0v?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video9_2() {
-    document.getElementById("video9").src = "https://www.googleapis.com/drive/v3/files/1uF3MNsfcP0_DGNatAFfY0LDf3R-GEdRd?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video10_2() {
-    document.getElementById("video10").src = "https://www.googleapis.com/drive/v3/files1TsDT8fN1EW1bBmXSmMSXEFAOpaUAZ7CD/?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video11_2() {
-    document.getElementById("video11").src = "https://www.googleapis.com/drive/v3/files/1wIG8-Y1Cckto8mPGS8ai8Tjn3INWXKn_?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
-}
-
-function video12_2() {
-    document.getElementById("video12").src = "https://www.googleapis.com/drive/v3/files/1odI6mI_b2IH70rrfzkVjp6OTi-e-LuDD?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media"
+function video12() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('Episode').innerHTML = '#12 父女之間 心心相連';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_12_end.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/索瑪麗與森林之神/索瑪麗與森林之神_1080P_12_end.mkv';
 }
