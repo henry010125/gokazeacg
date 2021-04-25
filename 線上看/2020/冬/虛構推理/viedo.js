2edea6fd0934234c55294b1ec9e638d121e7d59f
gokazeacg <gokazeacg@gmail.com>
2021-04-25 15:12:44 +0800

    Update viedo.js

--- 線上看/2020/冬/虛構推理/viedo.js
+++ 線上看/2020/冬/虛構推理/viedo.js
@@ -2,15 +2,33 @@ function dl() {
     location.href='https://www.googleapis.com/drive/v3/files/1TKJzFk4WGyXUwxXvb9Fm_dl-Rtc9awYs?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media'
 }
 
+function dfaults() {
+    document.getElementById('zero').className = 'btn btn-light selectep';
+    document.getElementById('one').className = 'btn btn-light selectep';
+    document.getElementById('two').className = 'btn btn-light selectep';
+    document.getElementById('three').className = 'btn btn-light selectep';
+    document.getElementById('four').className = 'btn btn-light selectep';
+    document.getElementById('five').className = 'btn btn-light selectep';
+    document.getElementById('six').className = 'btn btn-light selectep';
+    document.getElementById('seven').className = 'btn btn-light selectep';
+    document.getElementById('eight').className = 'btn btn-light selectep';
+    document.getElementById('nine').className = 'btn btn-light selectep';
+    document.getElementById('ten').className = 'btn btn-light selectep';
+    document.getElementById('eleven').className = 'btn btn-light selectep';
+    document.getElementById('twelve').className = 'btn btn-light selectep';
+}
+
 function home() {
     document.getElementById('home').style.display = 'unset';
     document.getElementById('video').style.display = 'none';
     document.getElementById('Episode').innerHTML = '故事大綱';
+    document.getElementById('zero').className = 'btn btn-primary selectep';
 }
 
 function video1() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('one').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#01 獨眼獨腳';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_01.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_01.mkv';
@@ -19,6 +37,7 @@ function video1() {
 function video2() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('two').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#02 沼神大蛇在傾聽';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_02.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_02.mkv';
@@ -27,6 +46,7 @@ function video2() {
 function video3() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('three').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#03 鋼人的傳聞';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_03.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_03.mkv';
@@ -35,6 +55,7 @@ function video3() {
 function video4() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('four').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#04 偶像死於鋼筋';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_04.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_04.mkv';
@@ -43,6 +64,7 @@ function video4() {
 function video5() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('five').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#05 想像力的怪物';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_05.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_05.mkv';
@@ -51,6 +73,7 @@ function video5() {
 function video6() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('six').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#06 合理的虛構';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_06.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_06.mkv';
@@ -59,6 +82,7 @@ function video6() {
 function video7() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('seven').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#07 鋼人攻略戰准備';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_07.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_07.mkv';
@@ -67,6 +91,7 @@ function video7() {
 function video8() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('eight').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#08 構築虛構之人';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_08.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_08.mkv';
@@ -75,6 +100,7 @@ function video8() {
 function video9() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('nine').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#09 鋼人七瀨攻略議會';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_09.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_09.mkv';
@@ -83,6 +109,7 @@ function video9() {
 function video10() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('ten').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#10 爭奪虛構';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_10.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_10.mkv';
@@ -91,6 +118,7 @@ function video10() {
 function video11() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('eleven').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#11 最後的虛構';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_11.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_11.mkv';
@@ -99,6 +127,7 @@ function video11() {
 function video12() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
+    document.getElementById('twelve').className = 'btn btn-primary selectep';
     document.getElementById('Episode').innerHTML = '#12 秩序守護者';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_12.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_12.mkv';
