ab0894e20c196004206beb4d8bbea484193de0db
gokazeacg <gokazeacg@gmail.com>
2021-05-07 14:07:30 +0800

    202004日

---
+++ 202004/社長，戰鬥的時間到了！/sc.js
@@ -0,0 +1,134 @@
+function dl() {
+    location.href='https://www.googleapis.com/drive/v3/files/1txAJPJIG8e1fDzpfbwuDvFl5mS7nrg6u?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media'
+}
+
+function dfaults() {
+    document.getElementById('zero').className = 'btn btn-light selectep';
+    document.getElementById('one').className = 'btn btn-light selectep';
+    document.getElementById('two').className = 'btn btn-light selectep';
+    document.getElementById('three').className = 'btn btn-light selectep';
+    document.getElementById('four').className = 'btn btn-light selectep';
+    document.getElementById('five').className = 'btn btn-light selectep';
+    document.getElementById('six').className = 'btn btn-light selectep';
+    document.getElementById('seven').className = 'btn btn-light selectep';
+    document.getElementById('eight').className = 'btn btn-light selectep';
+    document.getElementById('nine').className = 'btn btn-light selectep';
+    document.getElementById('ten').className = 'btn btn-light selectep';
+    document.getElementById('eleven').className = 'btn btn-light selectep';
+    document.getElementById('twelve').className = 'btn btn-light selectep';
+}
+
+function home() {
+    document.getElementById('home').style.display = 'unset';
+    document.getElementById('video').style.display = 'none';
+    document.getElementById('Episode').innerHTML = '故事大綱';
+    document.getElementById('zero').className = 'btn btn-primary selectep';
+}
+
+function video1() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('one').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#01 社長就任';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_01.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_01.mkv';
+}
+
+function video2() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('two').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#02 遺產繼承';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_02.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_02.mkv';
+}
+
+function video3() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('three').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#03 訂單比賽';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_03.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_03.mkv';
+}
+
+function video4() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('four').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#04 財寶探索';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_04.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_04.mkv';
+}
+
+function video5() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('five').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#05 新入社員';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_05.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_05.mkv';
+}
+
+function video6() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('six').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#06 企業目標';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_06.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_06.mkv';
+}
+
+function video7() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('seven').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#07 中期決算';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_07.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_07.mkv';
+}
+
+function video8() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('eight').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#08 新種發現';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_08.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_08.mkv';
+}
+
+function video9() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('nine').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#09 白馬騎士';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_09.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_09.mkv';
+}
+
+function video10() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('ten').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#10 企業戰士';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_10.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_10.mkv';
+}
+
+function video11() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('eleven').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#11 商機到來';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_11.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_11.mkv';
+}
+
+function video12() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('twelve').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#12 經營理念';
+    document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_12.mkv';
+    document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/04/社長,戰鬥的時間到了!/社長,戰鬥的時間到了!_1080P_12.mkv';
+}
