5047b9e757d0fa04470d265ddbb587da5eb8230c
henry0125 <84610844+henry0125@users.noreply.github.com>
2021-07-11 18:00:54 +0800

    刀劍神域_S3_P3

---
+++ 202004/刀劍神域_S3_P3/sc.js
@@ -0,0 +1,124 @@
+function dl() {
+    location.href='https://www.googleapis.com/drive/v3/files/1xs_YTBVeQnnIzkhyYRBv9Ja8Ku2UXctK?supportsAllDrives=true&supportsTeamDrives=true&key=AIzaSyCFXD7hsqD_zXh6Zt3Zd1bAHsJap3mvLvQ&alt=media'
+}
+
+function dfaults() {
+    document.getElementById('zero').className = 'btn btn-light selectep';
+    document.getElementById('one').className = 'btn btn-light selectep';
+    document.getElementById('two').className = 'btn btn-light selectep';
+    document.getElementById('three').className = 'btn btn-light selectep';
+    document.getElementById('four').className = 'btn btn-light selectep';
+    document.getElementById('five').className = 'btn btn-light selectep';
+    document.getElementById('six').className = 'btn btn-light selectep';
+    document.getElementById('seven').className = 'btn btn-light selectep';
+    document.getElementById('eight').className = 'btn btn-light selectep';
+    document.getElementById('nine').className = 'btn btn-light selectep';
+    document.getElementById('ten').className = 'btn btn-light selectep';
+    document.getElementById('eleven').className = 'btn btn-light selectep';
+}
+
+function home() {
+    document.getElementById('home').style.display = 'unset';
+    document.getElementById('video').style.display = 'none';
+    document.getElementById('Episode').innerHTML = '故事大綱';
+    document.getElementById('zero').className = 'btn btn-primary selectep';
+}
+
+function video1() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('one').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#01 裏世界大戰';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_37.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_37.mp4';
+}
+
+function video2() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('two').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#02 無限的盡頭';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_38.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_38.mp4';
+}
+
+function video3() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('three').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#03 煽動';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_39.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_39.mp4';
+}
+
+function video4() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('four').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#04 Code871';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_40.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_40.mp4';
+}
+
+function video5() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('five').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#05 惡魔之子';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_41.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_41.mp4';
+}
+
+function video6() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('six').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#06 記憶';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_42.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_42.mp4';
+}
+
+function video7() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('seven').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#07 覺醒';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_43.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_43.mp4';
+}
+
+function video8() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('eight').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#08 夜空之劍';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_44.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_44.mp4';
+}
+
+function video9() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('nine').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#09 時間的彼方';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_45.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_45.mp4';
+}
+
+function video10() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('ten').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#10 愛麗絲';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_46.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_46.mp4';
+}
+
+function video11() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('video').style.display = 'unset';
+    document.getElementById('eleven').className = 'btn btn-primary selectep';
+    document.getElementById('Episode').innerHTML = '#11 新世界';
+    document.getElementById('1080P').src = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_47.mp4';
+    document.getElementById('download').href = 'http://animeserver.gokazeacg.ga/AnimeServer_A/Anime/2020/07/刀劍神域_S3_P3/刀劍神域_Alicization_1080P_47.mp4';
+}
--- 範本/example.js
+++ 範本/example.js
@@ -29,7 +29,7 @@ function video1() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('one').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#01 獨眼獨腳';
+    document.getElementById('Episode').innerHTML = '#01 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_01.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_01.mkv';
 }
@@ -38,7 +38,7 @@ function video2() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('two').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#02 沼神大蛇在傾聽';
+    document.getElementById('Episode').innerHTML = '#02 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_02.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_02.mkv';
 }
@@ -47,7 +47,7 @@ function video3() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('three').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#03 鋼人的傳聞';
+    document.getElementById('Episode').innerHTML = '#03 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_03.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_03.mkv';
 }
@@ -56,7 +56,7 @@ function video4() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('four').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#04 偶像死於鋼筋';
+    document.getElementById('Episode').innerHTML = '#04 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_04.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_04.mkv';
 }
@@ -65,7 +65,7 @@ function video5() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('five').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#05 想像力的怪物';
+    document.getElementById('Episode').innerHTML = '#05 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_05.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_05.mkv';
 }
@@ -74,7 +74,7 @@ function video6() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('six').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#06 合理的虛構';
+    document.getElementById('Episode').innerHTML = '#06 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_06.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_06.mkv';
 }
@@ -83,7 +83,7 @@ function video7() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('seven').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#07 鋼人攻略戰准備';
+    document.getElementById('Episode').innerHTML = '#07 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_07.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_07.mkv';
 }
@@ -92,7 +92,7 @@ function video8() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('eight').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#08 構築虛構之人';
+    document.getElementById('Episode').innerHTML = '#08 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_08.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_08.mkv';
 }
@@ -101,7 +101,7 @@ function video9() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('nine').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#09 鋼人七瀨攻略議會';
+    document.getElementById('Episode').innerHTML = '#09 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_09.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_09.mkv';
 }
@@ -110,7 +110,7 @@ function video10() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('ten').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#10 爭奪虛構';
+    document.getElementById('Episode').innerHTML = '#10 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_10.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_10.mkv';
 }
@@ -119,7 +119,7 @@ function video11() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('eleven').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#11 最後的虛構';
+    document.getElementById('Episode').innerHTML = '#11 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_11.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_11.mkv';
 }
@@ -128,7 +128,7 @@ function video12() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('video').style.display = 'unset';
     document.getElementById('twelve').className = 'btn btn-primary selectep';
-    document.getElementById('Episode').innerHTML = '#12 秩序守護者';
+    document.getElementById('Episode').innerHTML = '#12 ';
     document.getElementById('1080P').src = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_12.mkv';
     document.getElementById('download').href = 'https://kazeindex.herokuapp.com/IndexB/Video/Anime/2020/01/虛構推理/虛構推理_1080P_12.mkv';
 }
